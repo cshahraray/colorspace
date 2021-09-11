012571ecdadfb9cc86d5cb4f9e3105d179256ee7
Kamran <cshahraray@gmail.com>
2021-09-10 21:51:10 -0400

    Begins router setup.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+const palettes = require("./routes/api/palettes");
+// const bodyParser = require('body-parser');
 
 const app = express();
+
 const db = require('./config/keys').mongoURI;
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -9,7 +13,16 @@ mongoose
   .catch(err => console.log(err));
 
 
-
+//configure port 
 const port = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//import routes 
 app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/users", users);   
+app.use("/api/palettes", palettes);
+
+//run server
 app.listen(port, () => console.log(`Server is running on port ${port}`));
